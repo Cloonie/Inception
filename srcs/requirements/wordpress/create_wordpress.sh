7cd6d8599d749d93a9cec558ace0731e35c89cd1
root <rsdoot@DESKTOP-T5CUUII>
2023-11-23 19:04:05 +0800

    502 bad gateway

--- srcs/requirements/wordpress/create_wordpress.sh
+++ srcs/requirements/wordpress/create_wordpress.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 if [ -f ./wp-config.php ]; then
-	echo "WordPress already installed"
+	echo "WordPress already installed."
 else
 	cd /var/www/html
 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp
+
 	wp core download --allow-root
 
 	cp wp-config-sample.php wp-config.php
